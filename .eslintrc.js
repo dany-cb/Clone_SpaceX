07643e44ca3e68ff5695f6236ed03e555757ae84
Daniel Thomas <danjes002@gmail.com>
2022-08-03 18:32:41 +0530

    build: add eslint prettier
    fixes #2

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended'
+    'prettier',
+  ],
+  plugins: ['import','@typescript-eslint', 'react', 'prettier'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'consistent-return': 'off',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'prettier/prettier': 0,
+  },
+}
