38b41ff855e783063c200b99c8ce6e204603e016
Daniel Thomas <danjes002@gmail.com>
2022-08-21 13:41:33 +0530

    feat: add css
    add styling and font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,54 @@
 import { BiMenu, BiX } from 'react-icons/bi'
+import { css } from '@emotion/react'
+
 import Logo from './Logo'
+
+const testBGBlack = css`
+  background-color: #000;
+`
+
+const btn = css`
+  cursor: pointer;
+  background: none;
+  border: none;
+  height: 26px;
+  position: absolute;
+  top: 20px;
+  right: 12px;
+  padding: 0; //not removed by reset-css
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+const header = css`
+  ${testBGBlack};
+  position: absolute;
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const hiddenMenu = css`
+  height: 100%;
+  width: 290px;
+  background-color: #000;
+  margin-left: auto;
+`
 
 function Header() {
   return (
-    <header>
+    <header css={header}>
       <Logo />
-      <nav>
+      <nav
+        css={css`
+          display: none;
+        `}
+      >
         <ul>
           <li>
             <a href="#">Falcon 9</a>
@@ -33,15 +76,50 @@ function Header() {
           </li>
         </ul>
       </nav>
-      <button>
+      <button css={btn}>
         <BiMenu color="#fff" />
       </button>
-      <div>
-        <button>
-          <BiX color="#fff" />
-        </button>
-        <nav>
-          <ul>
+      <div
+        css={css`
+          display: none;
+          position: absolute;
+          top: 0;
+          height: 100vh;
+          width: 100%;
+          background-color: rgba(0, 0, 0, 0.5);
+        `}
+      >
+        <nav css={hiddenMenu}>
+          <button css={btn}>
+            <BiX color="#fff" />
+          </button>
+          <ul
+            css={css`
+              padding-top: 60px;
+
+              li {
+                list-style: none;
+                border-bottom: solid 1px rgba(255, 255, 255, 0.18);
+              }
+
+              a {
+                padding: 12px;
+                padding-right: 18px;
+                display: block;
+                width: 100%;
+                text-align: end;
+                text-decoration: none;
+                cursor: pointer;
+                text-transform: uppercase;
+                color: #fff;
+                transition: color 0.5s;
+
+                &:hover {
+                  color: #8b939b;
+                }
+              }
+            `}
+          >
             <li>
               <a href="#">Falcon 9</a>
             </li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
+import React from 'react'
+import { Global, css } from '@emotion/react'
+
 import { Header } from '../components'
 
+const global = css`
+  @font-face {
+    font-family: 'DDIN';
+    font-style: normal;
+    font-weight: 400;
+    src: url(/fonts/D-DIN.woff) format('woff');
+  }
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'DDIN';
+  }
+`
+
 const IndexPage = () => {
-  return <Header />
+  return (
+    <>
+      <Global styles={global} />
+      <Header />
+    </>
+  )
 }
 
 export default IndexPage
